016724b59c8458fa36d4cf0c96a5cf3577a75b26
DPRuin <dpruin@126.com>
2015-10-30 17:58:52 +0800

    思路

--- BjShop/BjShop/Classes/Home(首页)/Controller/BJHomeViewController.m
+++ BjShop/BjShop/Classes/Home(首页)/Controller/BJHomeViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "BJHomeViewController.h"
+#import "BJMenuViewController.h"
+#import "UIViewController+MMDrawerController.h"
 
 @interface BJHomeViewController ()
 
@@ -16,23 +18,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+    
     self.title = @"bijia";
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(menuButtonClick)];
+    
+    // 拖拽手势
+    UIPanGestureRecognizer *recognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(dragging:)];
+    [self.view addGestureRecognizer:recognizer];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)dragging:(UIPanGestureRecognizer *)recognizer
+{
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)menuButtonClick
+{
+
 }
-*/
-
 @end
