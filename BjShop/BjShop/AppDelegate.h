757171bd14e30a698f306b34d3a9de82594c300a
DPRuin <dpruin@126.com>
2015-10-30 10:07:08 +0800

    初始化程序

---
+++ BjShop/BjShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BjShop
+//
+//  Created by BaiFaHui on 15/10/30.
+//  Copyright © 2015年 BaiFaHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BjShop/BjShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BjShop
+//
+//  Created by BaiFaHui on 15/10/30.
+//  Copyright © 2015年 BaiFaHui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
